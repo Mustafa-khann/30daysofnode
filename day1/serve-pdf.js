1c8f90accef6294cec096109e19f499adfa65b4a
Mustafa <mkafridi073@gmail.com>
2022-06-18 14:53:25 +0500

    serve-pdf with node-http server

--- day1/serve-pdf.js
+++ day1/serve-pdf.js
@@ -4,3 +4,24 @@ const fs = require('fs');
 var host = '127.0.0.1'
 var port = 3000
 
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {"Content-Type": "application/pdf"});
+
+    fs.readFile('timetable.pdf', (error, data) => {
+        if(error){
+            res.json({"Status": 'error', msg:err});
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+server.listen(port, host, (error) => {
+    if(error)
+    {
+        console.log("Error occured", error);
+    }
+    console.log(`Server running on ${host}:${port}`);
+});
