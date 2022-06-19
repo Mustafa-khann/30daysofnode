a3baa8774ee1cef3ad2c27d5ab17f55a0a08bad3
Mustafa <mkafridi073@gmail.com>
2022-06-19 13:06:01 +0500

    writing to file synchronously

---
+++ Day-2/write-fileSync.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+var content = "I am writing content to the file synchronously!";
+
+fs.writeFileSync('file.txt', content, (err) => {
+    if (err)
+    {
+        throw err
+    }
+    else
+    console.log("Synchronous writing to the file successful!");
+});
