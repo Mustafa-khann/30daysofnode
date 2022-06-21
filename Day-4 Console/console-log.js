69d3eb14b3e498c113ae6856e19eb12f11e19266
Mustafa <mkafridi073@gmail.com>
2022-06-21 10:20:08 +0500

    log in console

---
+++ Day-4 Console/console-log.js
@@ -0,0 +1,16 @@
+// using '' to print
+console.log('Hello there!');
+// using "" to print 
+console.log("2: This will work as well");
+
+var str = '30daysofnodechallenge';
+
+var val = 25;
+
+console.log(str);
+
+console.log('4: Value of val: %d', val);
+
+console.log('5: %s', 'this will be printed after 5');
+
+console.log("6: str = " + str);
