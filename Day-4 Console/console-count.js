e393a626bcaf6fbd0a07d0c4f3a57c90d7c5c512
Mustafa <mkafridi073@gmail.com>
2022-06-21 10:36:48 +0500

    console count

--- Day-4 Console/console-count.js
+++ Day-4 Console/console-count.js
@@ -1,1 +1,13 @@
 // The console.count() method logs the number of times that this particular call to count() has been called.
+
+console.count('Hello Old Friend');
+console.count('aRRR JAAY');
+console.count('teeee Jayyy');
+console.count('rj');
+console.count('c++');
+console.count('machine');
+console.count('bro');
+console.count('there');
+console.count('is a');
+console.count('monkey');
+console.count();
