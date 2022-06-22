586a62edaa275a875d389163a01ad179afb383aa
Mustafa <mkafridi073@gmail.com>
2022-06-22 11:57:02 +0500

    callbacks for Error

---
+++ Day-5 Errors/Error-callbacks.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+function nSCallBack(err, data)
+{
+    if(err)
+    {
+        console.error("Error : ", err);
+    }
+    else{
+        console.log(data);
+    }
+}
+
+fs.readFile('file.txt','utf-8' ,nSCallBack);
+fs.readFile('notfile.txt', nSCallBack);
