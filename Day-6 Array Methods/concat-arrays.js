e00e3d18875d7cda9b0a63ef8f8582b3f8783faa
Mustafa <mkafridi073@gmail.com>
2022-06-23 09:32:54 +0500

    concatinating two arrays

---
+++ Day-6 Array Methods/concat-arrays.js
@@ -0,0 +1,8 @@
+var arr = ['Apples', 'Bananas', 'Peaches'];
+var arr2 = ['Mangoes', 'Pineapple', 'Tomatoes'];
+
+console.log(arr);
+console.log(arr2);
+
+var new_array = arr.concat(arr2);
+console.log(new_array);
