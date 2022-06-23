a4f64c9a25803c79de927500629052cd356f818b
Mustafa <mkafridi073@gmail.com>
2022-06-23 09:36:22 +0500

    index of an element

---
+++ Day-6 Array Methods/indexOf.js
@@ -0,0 +1,7 @@
+var arr = [1,3,4,5,6,7,2]
+console.log(arr);
+
+var pos = arr.indexOf(6);
+console.log("The index of 6 is : ", pos);
+var pos2 =arr.indexOf(3);
+console.log("The index of 3 is : ", pos2);
