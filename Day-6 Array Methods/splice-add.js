bf6f9a4baf6fab66508b65e298320ed2dae1ff0e
Mustafa <mkafridi073@gmail.com>
2022-06-23 09:27:27 +0500

    Splice function

--- Day-6 Array Methods/splice-add.js
+++ Day-6 Array Methods/splice-add.js
@@ -1,4 +1,4 @@
 var arr = [1,3,4,5,6,7,2]
 console.log(arr);
-arr.splice(2,1,'rj','jakey');
+arr.splice(2,2,'rj','jakey');
 console.log(arr);
---
+++ Day-6 Array Methods/splice-remove.js
@@ -0,0 +1,4 @@
+var arr = [1,3,4,5,6,7,2]
+console.log(arr);
+arr.splice(2,3);
+console.log(arr);
