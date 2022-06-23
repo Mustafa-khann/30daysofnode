40bd29b7fc9ac199449c52fac0cbdaca4fb1fb8c
Mustafa <mkafridi073@gmail.com>
2022-06-23 09:57:35 +0500

    array joining of elements in a strng

---
+++ Day-6 Array Methods/array-join.js
@@ -0,0 +1,8 @@
+var arr = [1,3,4,5,6,7,2]
+var arr1 = ['a','b','c','d','e','f','g'];
+console.log(arr);
+console.log(arr1);
+var str = arr.join(" ");
+var str2 = arr1.join(':');
+console.log(str);
+console.log(str2);
