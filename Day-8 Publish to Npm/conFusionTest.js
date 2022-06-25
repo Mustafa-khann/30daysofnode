467bb8b7869e6a5f69008c0767ef711701591461
Mustafa <mkafridi073@gmail.com>
2022-06-25 10:54:55 +0500

    All done for day-8

--- Day-8 Publish to Npm/conFusionTest.js
+++ Day-8 Publish to Npm/conFusionTest.js
@@ -1,8 +1,9 @@
-const arthematicOps = require('./arthematic-Operations');
+const arthematicOps = require('./arthematics-operations');
 var addition = arthematicOps.Addition(12,34);
 console.log(addition);
 var subtraction = arthematicOps.Subtraction(34,12);
 console.log(subtraction);
 var multiplication = arthematicOps.Multiplication(23,2);
 console.log(multiplication);
-var division = arthematicOps.Division()
+var division = arthematicOps.Division(40,2)
+console.log(division);
