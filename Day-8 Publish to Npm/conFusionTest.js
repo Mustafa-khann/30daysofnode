3e682343d1219b05e2c3c6c291609f5cc6b89f3b
Mustafa <mkafridi073@gmail.com>
2022-06-25 10:53:15 +0500

    importing that arthematic operation module.

---
+++ Day-8 Publish to Npm/conFusionTest.js
@@ -0,0 +1,8 @@
+const arthematicOps = require('./arthematic-Operations');
+var addition = arthematicOps.Addition(12,34);
+console.log(addition);
+var subtraction = arthematicOps.Subtraction(34,12);
+console.log(subtraction);
+var multiplication = arthematicOps.Multiplication(23,2);
+console.log(multiplication);
+var division = arthematicOps.Division()
