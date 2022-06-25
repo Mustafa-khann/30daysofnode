8580609aa7a861f0ad51443c3f5322a35e7d966d
Mustafa <mkafridi073@gmail.com>
2022-06-25 10:52:55 +0500

    arthematic operation module

---
+++ Day-8 Publish to Npm/arthematics-operations.js
@@ -0,0 +1,26 @@
+function addition(x,y){
+    var sum = x+y;
+    return sum;
+}
+
+function subtraction(x,y){
+    var sub = x-y;
+    return sub;
+}
+
+function multiplication(x,y){
+    var product = x*y;
+    return product;
+}
+
+function division(x,y){
+    var div = x/y;
+    return div;
+}
+
+module.exports = {
+    Addition: addition,
+    Subtraction: subtraction,
+    Multiplication: multiplication,
+    Division: division,
+}
