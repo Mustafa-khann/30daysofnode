72d30670a07c0a50933676bcda17bdfb6e042166
Mustafa <mkafridi073@gmail.com>
2022-06-26 12:12:23 +0500

    All done for Day-9

--- Day-9 Hashing/file_hmac.js
+++ Day-9 Hashing/file_hmac.js
@@ -11,7 +11,6 @@ const file_data = fs.ReadStream(filename);
 file_data.on('data', function(data){
     hmac.update(data);
 })
-
 file_data.on('end', function(){
     console.log("HMAC generated using " + algorithm + '\nHashed output is ' + hmac.digest('hex'), '\nFilename is '+ filename);
     fs.writeFileSync(filename, hmac.digest('hex'));
