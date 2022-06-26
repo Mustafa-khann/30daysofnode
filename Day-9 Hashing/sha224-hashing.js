0bc72c9f2b5b27c445eb97dcdd0254817eef2ea0
Mustafa <mkafridi073@gmail.com>
2022-06-26 11:26:24 +0500

    sha224 hashing algorithm

---
+++ Day-9 Hashing/sha224-hashing.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto');
+
+const hash = crypto.createHash('sha224');
+
+const data = hash.update('#30daysofnode', 'utf-8');
+
+const gen_hash = data.digest('hex');
+
+console.log("Hash: ", gen_hash);
