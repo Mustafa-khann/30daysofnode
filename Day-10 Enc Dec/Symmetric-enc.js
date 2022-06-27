c4853eb1135127b134a574b3680fe35dfd32d1c2
Mustafa <mkafridi073@gmail.com>
2022-06-27 16:47:15 +0500

    Symmetric Encryption

---
+++ Day-10 Enc Dec/Symmetric-enc.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto'),algorithm = "aes-256-ctr", password = "mustafa";
+
+function encrypt (text)
+{
+    var cipher = crypto.createCipher(algorithm, password);
+    var crypted = cipher.update(text, 'utf-8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function decrypt (text)
+{
+    var decipher = crypto.createDecipher(algorithm,password);
+    var decrypted = decipher.update(text, 'hex','utf-8');
+    decipher += decipher.final('utf-8');
+    return decrypted;
+}
+
+var text = "This 30daysofnode challenge is amazing. I've learned alot.";
+
+var e = encrypt(text);
+console.log("Encrypted String: ",e);
+
+var d = decrypt(e);
+console.log("Decrypted String",d);
