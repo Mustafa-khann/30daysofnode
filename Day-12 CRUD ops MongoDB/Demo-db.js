bd36dcfe59dd366852a6c5d6988e42069bae8b5c
Mustafa <mkafridi073@gmail.com>
2022-06-29 15:05:07 +0500

    CRUD on MongoDB

--- Day-12 CRUD ops MongoDB/Demo-db.js
+++ Day-12 CRUD ops MongoDB/Demo-db.js
@@ -6,6 +6,31 @@ async function main(){
     const client = new MongoClient(uri);
     try{
         await client.connect();
+        await listDatabases(client);
+        await createMultipleListings(client, await createMultipleListings(client, [
+            {
+                name: "Infinite Views",
+                summary: "Modern home with infinite views from the infinity pool",
+                property_type: "House",
+                bedrooms: 5,
+                bathrooms: 4.5,
+                beds: 5
+            },
+            {
+                name: "Private room in London",
+                property_type: "Apartment",
+                bedrooms: 1,
+                bathroom: 1
+            },
+            {
+                name: "Beautiful Beach House",
+                summary: "Enjoy relaxed beach living in this house with a private beach",
+                bedrooms: 4,
+                bathrooms: 2.5,
+                beds: 7,
+                last_review: new Date()
+            }
+        ]));
     }
     catch(err){
         throw err;
@@ -15,3 +40,21 @@ async function main(){
 }
 
 main().catch(console.error);
+
+async function createMultipleListings(client, newListings){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListing);
+    console.log(`${result.insertedCount} new listings created with the following ids: `);
+    console.log(result.insertedIds);
+}   
+
+async function createListing(client, newListing){
+   const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing inserted with the following id: ${result.insertedId}`);
+}
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases: ");
+    databasesList.databases.forEach(db => {
+        console.log(`- ${db.name}`);
+    });
+}
