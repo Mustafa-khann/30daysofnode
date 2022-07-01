afd2a04803c90865f7c6ae51b37f0a67a79fdefb
Mustafa <mkafridi073@gmail.com>
2022-07-01 16:11:21 +0500

    minor changes

--- Day-13 SignUp form/server.js
+++ Day-13 SignUp form/server.js
@@ -3,14 +3,65 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const crypto = require('crypto');
 const path = require('path');
-const mongo = require('mongodb');
+const {MongoClient} = require('mongodb');
 
 var app = express();
-const db = "mongodb://127.0.0.1:27017/users"
+const port = 3000;
+const uri = "mongodb://127.0.0.1:27017/users"
 
 app.get('/', (req,res) =>{
     res.set({
         'Access-Control-Allow-Origin': '*'
     });
     return res.redirect('/public/index.html')
+}).listen(port);
+
+console.log(`Server running on port: ${port}`);
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.post('/sign_up', (req,res)=> {
+    var name = req.body.name;
+    var email = req.body.email;
+    var pass = req.body.password;
+    var phone = req.body.phone;
+    // hashing the password
+    var password = getHash(pass,phone);
+
+    var data = {
+        "name": name,
+        "email": email,
+        "password": pass,
+        "phone": phone
+    }
+    MongoClient.connect(uri , function(error , db){
+		if (error){
+			throw error;
+		}
+		console.log("connected to database successfully");
+		//CREATING A COLLECTION IN MONGODB USING NODE.JS
+		db.collection("details").insertOne(data, (err , collection) => {
+			if(err) throw err;
+			console.log("Record inserted successfully");
+			console.log(collection);
+		});
+	});
+
+    console.log("Data is ", JSON.stringify(data));
+    res.set({
+        'Access-Control-Allow-Origin' : '*'
+    })
+    res.redirect('/public/success.html');
 })
+
+var getHash = (pass, phone) => {
+    var hmac = crypto.createHmac('sha512', phone);
+
+    // passing the data to be hashed
+    data = hmac.update(pass);
+    gen_hmac = data.digest('hex');
+    console.log('Hmac', gen_hmac);
+    return gen_hmac;
+}
