540bd408db1e0a57369d649a9476b944e984ca28
Mustafa <mkafridi073@gmail.com>
2022-07-02 19:08:29 +0500

    All done for Day-14

---
+++ Day-14 Socket IO/server.js
@@ -0,0 +1,24 @@
+const { emitWarning } = require('process');
+
+const app = require('express')();
+var server = require('http').Server(app);
+const io = require('socketio')(server);
+server.listen(3000);
+console.log('Server listening on port 3000');
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/admin', (req,res) => {
+    res.sendFile(__dirname + '/admin.html');
+})
+
+
+io.on('connection', (socket)=>{
+    socket.emit('welcome', {data: "welcome"});
+
+    socket.on('new', (data) => {
+        console.log('About to upload Quote');
+        io.socket.emit('next', {data: data});
+    })
+})
