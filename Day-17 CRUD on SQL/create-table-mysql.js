62504df022bcf0ca60a3027b73777bed11f78552
Mustafa <mkafridi073@gmail.com>
2022-07-05 19:20:03 +0500

    table Creation

--- Day-17 CRUD on SQL/create-table-mysql.js
+++ Day-17 CRUD on SQL/create-table-mysql.js
@@ -3,12 +3,12 @@ const mysql = require('mysql');
 var connect = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'mydb'
+    password: 'root',
+    database: 'mysql'
 });
 
 var table = 'CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))';
-connect.query(table, function(err, result) {
-    if (err) throw err;
-    console.log('Table created');
+connect.query(table, (err, result) => {
+    if (err) {throw err;}
+    console.log('Table created Successfully');
 });
