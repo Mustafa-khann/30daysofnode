6f5e47c6748db2112e277dd49bad556dba4f8434
Mustafa <mkafridi073@gmail.com>
2022-07-06 12:35:03 +0500

    minor changes

--- Day-18 Callbacks/non-blocking-code.js
+++ Day-18 Callbacks/non-blocking-code.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
+const { start } = require('repl');
 
 
 //For calculating execution time
 var date1 = new Date();
-var time_start = date1.getTime();
+var start_time = date1.getTime();
 console.log("{Check point 1} starting at: " + time_start);
 console.log("Let's start reading the file");
 
 // Reading the file
-var filename = fs.readFileSync(file.txt)
+var filename = 'file.txt';
+
+fs.readFile('output.txt', (err, data) => {
+    if(err)
+    {
+        throw err;
+    }
+    else
+    {
+        console.log(data);
+    }
+});
+
+const date2 = new Date();
+const end_time = date2.getTime();
+var execution_time = end_time - start_time;
+console.log('Execution time: ', execution_time);
+
+
