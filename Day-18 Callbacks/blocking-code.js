0719d3bd64a9857029410090c00d0cd34f27112f
Mustafa <mkafridi073@gmail.com>
2022-07-06 12:19:30 +0500

    minor changes

--- Day-18 Callbacks/blocking-code.js
+++ Day-18 Callbacks/blocking-code.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+
+const date1 = new Date();
+const start_time = date1.getTime();
+console.log('Starting at: ', start_time);
+console.log('Lets start reading file.');
+
+var filename = 'file.txt';
+var content = fs.readFileSync(filename);
+
+console.log('Content: ', content);
