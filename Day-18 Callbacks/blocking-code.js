d3f3a287d7cf6f6f5036402bf4a7e96adafe387a
Mustafa <mkafridi073@gmail.com>
2022-07-06 12:28:29 +0500

    minor changes

--- Day-18 Callbacks/blocking-code.js
+++ Day-18 Callbacks/blocking-code.js
@@ -1,11 +1,21 @@
+const { timeEnd } = require('console');
 const fs = require('fs');
 
+// for calculating Execution time
 const date1 = new Date();
 const start_time = date1.getTime();
 console.log('Starting at: ', start_time);
 console.log('Lets start reading file.');
-
+// for reading the file
 var filename = 'file.txt';
 var content = fs.readFileSync(filename);
-
 console.log('Content: ', content);
+
+// for calculating Execution time
+var date2 = new Date();
+var end_time = date2.getTime();
+console.log('Finishing at: ', end_time);
+var execution_time = end_time - start_time;
+console.log("Execution time was : ", execution_time, 'ms');
+
+console.log('Another task to be Executed');
