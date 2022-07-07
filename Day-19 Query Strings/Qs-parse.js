c1d3db20a6afb6160cbbb69fd11c2d125f209757
Mustafa <mkafridi073@gmail.com>
2022-07-07 19:58:48 +0500

    All done for Day-19

---
+++ Day-19 Query Strings/Qs-parse.js
@@ -0,0 +1,9 @@
+const querystring = require('querystring');
+
+var value_json_2 = querystring.parse('id=1&name=mk&name=30days&challenge=30daysofnode&creator%nodejs','&','%');
+console.log("Changing the default 'eq' from '=' to '%' an example is shown below:");
+console.log(value_json_2);
+
+var value_json_3 = querystring.parse('id%3#name%emily#name%30daysofnode#challenge%30daysofnode#creator%mustafa','#','%');
+console.log("Changing the default Separator 'sep' from '&' to '#'. An example is shown below:")
+console.log(value_json_3);
