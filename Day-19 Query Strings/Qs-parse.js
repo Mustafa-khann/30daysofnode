f8b70f0def6891b37844ce5f361d4b06d5398480
Mustafa <mkafridi073@gmail.com>
2022-07-07 20:00:29 +0500

    All done for Day-19

--- Day-19 Query Strings/Qs-parse.js
+++ Day-19 Query Strings/Qs-parse.js
@@ -1,6 +1,6 @@
 const querystring = require('querystring');
 
-var value_json_2 = querystring.parse('id=1&name=mk&name=30days&challenge=30daysofnode&creator%nodejs','&','%');
+var value_json_2 = querystring.parse('id=1&name=mk&name=30days&challenge=30daysofnode&creator%nodejs');
 console.log("Changing the default 'eq' from '=' to '%' an example is shown below:");
 console.log(value_json_2);
 
