59a243c8ab2ac30718cabda4f4f6c78ae9d815fc
Mustafa <mkafridi073@gmail.com>
2022-07-12 11:20:51 +0500

    All done for Day-24

--- Day-24 Multithreading/master-spawn.js
+++ Day-24 Multithreading/master-spawn.js
@@ -1,6 +1,18 @@
 const cp = require('./slave');
 
 for (let i = 0; i < 6; i++) {
-   var worker = cp.spawn;
+   var worker = cp.spawn('node', ['slave.js', i]);
+
+   worker.stdout.on('data', function (data) {
+       console.log('Value of stdout: ', data);
+   });
+
+   worker.sdterr.on('data', function (data){
+       console.log('stderr' + data);
+   });
+
+   worker.stdin.on('close', (code) => {
+       console.log('Child process exited with code');
+   });
     
 }
