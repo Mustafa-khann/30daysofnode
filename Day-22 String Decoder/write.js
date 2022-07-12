cfc8d82bd6a121c67417966691bc80e7a16bb39f
Mustafa <mkafridi073@gmail.com>
2022-07-12 11:12:38 +0500

    All done for Day-24

--- Day-22 String Decoder/write.js
+++ Day-22 String Decoder/write.js
@@ -1,6 +1,6 @@
 const stringDecoder = require('string_decoder').StringDecoder;
 
-var sd = new StringDecoder('utf8');
+var sd = new stringDecoder('utf8');
 var buff = Buffer('data to be buffered');
 
 console.log(buff);
