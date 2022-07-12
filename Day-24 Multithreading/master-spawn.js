fca3972a73a3e8b109c89a3ba2833028d49f38ee
Mustafa <mkafridi073@gmail.com>
2022-07-12 11:17:12 +0500

    All done for Day-24

---
+++ Day-24 Multithreading/master-spawn.js
@@ -0,0 +1,6 @@
+const cp = require('./slave');
+
+for (let i = 0; i < 6; i++) {
+   var worker = cp.spawn;
+    
+}
--- Day-24 Multithreading/slave.js
+++ Day-24 Multithreading/slave.js
@@ -1,1 +1,2 @@
 console.log("Child Process number ", process.argv[2], "is executed.");
+
