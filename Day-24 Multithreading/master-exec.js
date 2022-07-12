a4542599626a360a5e2c18792ec269dc28fe04b9
Mustafa <mkafridi073@gmail.com>
2022-07-12 11:41:46 +0500

    All done for Day-24

--- Day-24 Multithreading/master-exec.js
+++ Day-24 Multithreading/master-exec.js
@@ -1,19 +1,20 @@
 const cp = require('child_process');
 
 for (let i = 0; i < 6; i++) {
-var workerProcess = cp.exec('node slave.js', i, function(error, stdout, stderr) {
-    if(error){
-        console.log(error.stack);
-        console.log('Error Code', error.code);
-        console.log('Error Signal', error.signal);
+
+var workerProcess = cp.exec('node slave.js ' + i , function(error, stdout, stderr) {
+    if (error) {
+       console.log(error.stack);
+       console.log('Error Code: '+error.code);
+       console.log('Error Signal: '+error.signal);
     }
 
-    if(stderr)
-    {
-        console.log('value of stderr: ', stderr);
+    if(stderr){
+       console.log('value of stderr: ' + stderr);
     }
-    console.log('Value of stdout: ', stdout);
-});
+
+    console.log('Value of stdout: ' + stdout); 
+ });
 workerProcess.on('exit', (code) => {
     console.log("Child process exited with code: ", code);
 });
