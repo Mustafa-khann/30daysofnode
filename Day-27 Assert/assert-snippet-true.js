d6eb941320ff37cb16e6541ed357f93f9105798d
Mustafa <mkafridi073@gmail.com>
2022-07-15 14:40:26 +0500

    All done for Day-27

--- Day-27 Assert/assert-snippet-true.js
+++ Day-27 Assert/assert-snippet-true.js
@@ -1,11 +1,12 @@
 const assert = require('assert');
 
-function demo(x,y,x){
+function demo(x,y,z){
     var value = x+y+z;
     return value;
 }
 
 var output = demo(5,5,5);
 var expected_output = 15;
+console.log("Output: ", output);
 console.log("Expected output: ", expected_output);
 assert(output==expected_output,'Test case is true so this won\'t printed');
