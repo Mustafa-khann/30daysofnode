12d020066a4d4b08b62e315f14a042da9b1fa5b8
Mustafa <mkafridi073@gmail.com>
2022-07-17 19:22:21 +0500

    All done for Day-29

--- Day-29 Dropbox file Upload/dropbox-File-Upload.js
+++ Day-29 Dropbox file Upload/dropbox-File-Upload.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 //enter your access token
 var access_token = "sl.BLlWZDkPkDZ6tfRw25VVTY6X_-PMwWqeLGoPoe7ePw3Vbrbbihzppg3QOCQp0yb4OCdHob50pmxJN1Ylgxwy1jn3X537JDZN2s_2Heprg0Z3T0zYWUAdX6CXW1Gpx7RuapFu_1o";
 //Name of the file to be uploaded
-var filename = '44.png';
+var filename = 'file.txt';
 //reading the contents 
 var content = fs.readFileSync(filename);
 //write your folder name in place of YOUR_PATH_TO_FOLDER
