860e7d5c44f44b88ad61cd753aa8f999570fb743
Mustafa <mkafridi073@gmail.com>
2022-07-17 19:35:33 +0500

    All done for Day-29

--- Day-29 Dropbox file Upload/dropbox-File-Upload.js
+++ Day-29 Dropbox file Upload/dropbox-File-Upload.js
@@ -1,29 +1,28 @@
-var request = require('request');
-var fs = require('fs');
+const https = require("https");
+fs = require('fs')
+const TOKEN = process.env.ACCESS_TOKEN;
+fs.readFile('file.txt', 'utf8', function (err, data) {
+	const req = https.request('https://content.dropboxapi.com/2/files/upload', {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${TOKEN}`,
+			'Dropbox-API-Arg': JSON.stringify({
+				'path': '/Upload/file.txt',
+				'mode': 'overwrite',
+				'autorename': true, 
+				'mute': false,
+				'strict_conflict': false
+			}),
+	    	'Content-Type': 'application/octet-stream',
+		}
+	}, (res) => {
+		console.log("statusCode: ", res.statusCode);
+	    console.log("headers: ", res.headers);
 
-//enter your access token
-var access_token = "sl.BLlWZDkPkDZ6tfRw25VVTY6X_-PMwWqeLGoPoe7ePw3Vbrbbihzppg3QOCQp0yb4OCdHob50pmxJN1Ylgxwy1jn3X537JDZN2s_2Heprg0Z3T0zYWUAdX6CXW1Gpx7RuapFu_1o";
-//Name of the file to be uploaded
-var filename = 'file.txt';
-//reading the contents 
-var content = fs.readFileSync(filename);
-//write your folder name in place of YOUR_PATH_TO_FOLDER
-// For example if the folder name is njera then we can write it in the following way :
-// "Dropbox-API-Arg": "{\"path\": \"/njera/"+filename+"\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}"
-options = {
-            method: "POST",
-            url: 'https://content.dropboxapi.com/2/files/upload',
-            headers: {
-              "Content-Type": "application/octet-stream",
-              "Authorization": "Bearer " + access_token,
-              "Dropbox-API-Arg": "{\"path\": \"/YOUR_PATH_TO_FOLDER/"+filename+"\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}",
-            },
-            body:content
-};
-
-request(options,function(err, res,body){
-     console.log("Err : " + err);
-     console.log("res : " + res);
-     console.log("body : " + body);    
- })
-	
+	    res.on('data', function(d) {
+	        process.stdout.write(d);
+	    });
+	});
+	req.write(data);
+	req.end();
+});
