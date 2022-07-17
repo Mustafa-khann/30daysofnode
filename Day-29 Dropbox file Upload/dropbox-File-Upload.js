2b5a9ae3dc06ccced1dd7939c873b2e39adc18c8
Mustafa <mkafridi073@gmail.com>
2022-07-17 15:42:18 +0500

    All done for Day-29

--- Day-29 Dropbox file Upload/dropbox-File-Upload.js
+++ Day-29 Dropbox file Upload/dropbox-File-Upload.js
@@ -7,4 +7,20 @@ const file_name = 'file.txt';
 
 var content = fs.readFileSync(file_name);
 
-option 
+option = {
+    method: "POST",
+    url: 'https://content.dropboxapi.com/2/files/uplaod',
+    header:
+    {
+        "Content-Type": "Application/octet-stream",
+        "Authorization": "Bearer" + access_token,
+        "Dropbox-API-Arg": "{\"path\": \"/./"+file_name+"\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}",
+    },
+    body:content
+};
+
+request(options, (err, res, body) => {
+    console.log("Err" + err);
+    console.log("res" + res);
+    console.log("body" + body);
+})
