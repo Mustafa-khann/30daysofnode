057a88392402be8439bce959b02d256a026f3e59
Mustafa <mkafridi073@gmail.com>
2022-07-17 16:31:03 +0500

    All done for Day-29

--- Day-29 Dropbox file Upload/dropbox-File-Upload.js
+++ Day-29 Dropbox file Upload/dropbox-File-Upload.js
@@ -1,19 +1,29 @@
 const https = require("https");
-const data = "{\"limit\": 1000}";
+fs = require('fs')
 const TOKEN = "sl.BLlWZDkPkDZ6tfRw25VVTY6X_-PMwWqeLGoPoe7ePw3Vbrbbihzppg3QOCQp0yb4OCdHob50pmxJN1Ylgxwy1jn3X537JDZN2s_2Heprg0Z3T0zYWUAdX6CXW1Gpx7RuapFu_1o"
-const req = https.request('https://api.dropboxapi.com/2/file_requests/count', {
-	method: 'POST',
-	headers: { 
-		'Authorization': `Bearer ${TOKEN}`,
-	},
-	
-}, (res) => {
-	console.log("statusCode: ", res.statusCode);
-    console.log("headers: ", res.headers);
+fs.readFile('file.txt', 'utf8', function (err, data) {
+	const req = https.request('https://content.dropboxapi.com/2/files/upload', {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${TOKEN}`,
+			'Dropbox-API-Arg': JSON.stringify({
+				'path': '/Upload/file.txt',
+				'mode': 'overwrite',
+				'autorename': true, 
+				'mute': false,
+				'strict_conflict': false
+			}),
+	    	'Content-Type': 'application/octet-stream',
+		}
+	}, (res) => {
+		console.log("statusCode: ", res.statusCode);
+	    console.log("headers: ", res.headers);
 
-    res.on('data', function(d) {
-        process.stdout.write(d);
-    });
-})
+	    res.on('data', function(d) {
+	        process.stdout.write(d);
+	    });
+	});
 
-req.end();
+	req.write(data);
+	req.end();
+});
