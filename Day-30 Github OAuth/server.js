b0eb8c461c19b8b83318a400f1ad8b88266ff28b
Mustafa <mkafridi073@gmail.com>
2022-07-18 21:47:31 +0500

    All done for Day-30

--- Day-30 Github OAuth/server.js
+++ Day-30 Github OAuth/server.js
@@ -3,10 +3,18 @@ const app = express();
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
 
+app.use(passport.initialize());
+
+passport.serializeUser((user, cb)=>{
+  cb(null, user.id);
+});
+passport.deserializeUser((id, cb)=>{
+  cb(null, id);
+});
 passport.use(new GitHubStrategy({
     clientID: "c62cd6617e4cf4acee73",
-    clientSecret: "9860a4921379009936c9addb3c8fd633ed99cc46",
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    clientSecret: "db797d010dfa48b66e54b3d97c393db25eefaa7a",
+    callbackURL: "http://localhost:3000/api/auth/github"
   },
   function(accessToken, refreshToken, profile, cb) {
       console.log(profile);
@@ -14,19 +22,19 @@ passport.use(new GitHubStrategy({
   }
 ));
 
-app.get('/', (req, res) => {
-    res.send(__dirname + '/dashboard.html');
+app.get('/dashboard', (req, res) => {
+    res.sendFile(__dirname + '/dashboard.html');
 })
 
 app.get('/login', (req, res) => {
-    res.send(__dirname + '/login');
+    res.sendFile(__dirname + '/login.html');
 
 })
 // auth
 app.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ] }));
 
-app.get('/auth/github/callback', 
+app.get('/api/auth/github', 
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
